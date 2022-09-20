f29b839c1fb394570590dd2a7b3f81206bd5a41d
Muhammadjon01 <75600542+Muhammadjon01@users.noreply.github.com>
2022-09-20 17:35:27 +0500

    I push to Show Completed part!

--- src/container/Todos/Todos.jsx
+++ src/container/Todos/Todos.jsx
@@ -5,6 +5,7 @@ import Todo from "../../components/Todo/Todo";
 function Todos() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -34,6 +35,14 @@ function Todos() {
     setTodos(updateTodo);
   };
 
+  const allStatus = () => {
+    setStatus("all");
+  };
+
+  const completeStatus = () => {
+    setStatus("complete");
+  };
+
   return (
     <div className="bg-blue-700">
       <div className="w-[80%] h-screen sm:ml-20 bg-white-600">
@@ -56,11 +65,31 @@ function Todos() {
               Add
             </button>
           </div>
+          <div className="flex justify-between bg-white px-2 py-4">
+            <button
+              className="rounded-lg px-2 py-1 bg-blue-500 shadow-lg hover:shadow-blue-500/50"
+              onClick={allStatus}
+            >
+              All
+            </button>
+            <button className="rounded-lg px-2 py-1 bg-blue-500 shadow-lg hover:shadow-blue-500/50">
+              Active
+            </button>
+            <button
+              className="rounded-lg px-2 py-1 bg-blue-500 shadow-lg hover:shadow-blue-500/50"
+              onClick={completeStatus}
+            >
+              Complete
+            </button>
+
+            <input type="search" placeholder="Search..." className="rounded"/>
+          </div>
         </div>
 
         <div className="lg:w-[50%]  bg-white rounded mx-auto py-10">
           <div className="bg-white space-y-2 md:mx-auto mx-auto my-4 px-4">
-            {todos.length > 0 &&
+            {status === "all" &&
+              todos.length > 0 &&
               todos.map((elem) => {
                 return (
                   <Todo
@@ -72,6 +101,22 @@ function Todos() {
                   />
                 );
               })}
+
+            {status === "complete" &&
+              todos.length > 0 &&
+              todos
+                .filter((elem) => elem.isCompleted)
+                .map((elem) => {
+                  return (
+                    <Todo
+                      key={elem.id}
+                      text={elem.text}
+                      isCompleted={elem.isCompleted}
+                      deleteTodo={() => deleteTodo(elem.id)}
+                      completeTodo={() => completeTodo(elem.id)}
+                    />
+                  );
+                })}
           </div>
         </div>
       </div>
